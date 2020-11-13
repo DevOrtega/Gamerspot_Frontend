b9811b0c3c0654e17b30df85554d0dbf08d50310
David Rodri <drodrigerrard@gmail.com>
2020-11-13 21:24:27 +0000

    Fix #5 profile view, logic, service and interfaces created. user interface

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
+import { ProfileComponent } from './pages/profile/profile.component';
 import { RegisterComponent } from './pages/register/register.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path:'login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
-  { path:'home', component: HomeComponent }
+  { path:'home', component: HomeComponent },
+  { path:'profile', component: ProfileComponent }
 ];
 
 @NgModule({
