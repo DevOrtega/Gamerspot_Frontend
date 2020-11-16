26fce99b4d1f4f4fee80e9866d20b121531b6c50
David Rodri <drodrigerrard@gmail.com>
2020-11-16 14:49:42 +0000

    Fix #12 async token, refresh-token (W I P)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { RegisterComponent } from './pages/register/register.component';
 import { ResetPasswordComponent } from './pages/reset-password/reset-password.component';
 
 const routes: Routes = [
+  //{ path: '', redirectTo: '/login', pathMatch: 'full' },
   { path:'login', component: LoginComponent },
   { path:'register', component: RegisterComponent },
   //{ path:'', component: HomeComponent },
