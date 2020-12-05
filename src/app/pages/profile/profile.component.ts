5f14b7468ea0a828f20122237882207b56c9275b
David Rodri <drodrigerrard@gmail.com>
2020-12-05 03:31:19 +0000

    Fix #48 profilephotos firebase & mongo

--- src/app/pages/profile/profile.component.ts
+++ src/app/pages/profile/profile.component.ts
@@ -1,12 +1,14 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { first } from 'rxjs/operators';
+import { first, map } from 'rxjs/operators';
 import { Subscription } from 'rxjs';
 import { AuthService } from 'src/app/services/auth/auth.service';
 import { UsersService } from 'src/app/services/users/users.service';
 import { User } from '../../interfaces/user'
 import { FeedsService } from 'src/app/services/feeds/feeds.service';
 import { Post } from 'src/app/interfaces/post';
+import { Profilephotos } from 'src/app/interfaces/profilephotos';
+import { ProfilephotosService } from 'src/app/services/firebase/profilephotos.service';
 
 @Component({
   selector: 'app-profile',
@@ -28,16 +30,24 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
   activeButton: string;
 
+  public profilePhotos: Profilephotos[];
+  public noPhoto: boolean;
+  private photoSubscription: Subscription;
+  private editPhotoSubscription: Subscription;
+
+  public loading:boolean = false;
+
   constructor(
     private router: Router,
     private route: ActivatedRoute,
     private authService: AuthService,
     private userService: UsersService,
-    private postService: FeedsService
+    private postService: FeedsService,
+    private photoService: ProfilephotosService
   ) {
     this.activeButton = 'btn1';
   }
-    
+
   ngOnInit(): void {
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
 
@@ -58,6 +68,7 @@ export class ProfileComponent implements OnInit, OnDestroy {
       this.isTeam = this.userService.isTeam(this.userProfileData);
       this.isSponsor = this.userService.isSponsor(this.userProfileData);
       this.itsMe = this.authService.itsMe(this.userProfileData.username);
+      if (this.userProfileData.photoUrl) this.showPhoto();
     } else {
       this.getUserSubscriptor = this.userService.getUserByUsername(this.usernameParam)
       .pipe(first())
@@ -75,6 +86,8 @@ export class ProfileComponent implements OnInit, OnDestroy {
           this.isTeam = this.userService.isTeam(this.userProfileData);
           this.isSponsor = this.userService.isSponsor(this.userProfileData);
           this.itsMe = this.authService.itsMe(this.userProfileData.username);
+
+          if (this.userProfileData.photoUrl) this.showPhoto();
         }
       })
     }
@@ -82,6 +95,38 @@ export class ProfileComponent implements OnInit, OnDestroy {
     this.getPostsSubscriptor = this.postService.getPosts(this.usernameParam)
     .pipe(first())
     .subscribe((posts: Post[]) => this.userPostsData = posts)
+
+
+  }
+
+  public showPhoto() {
+    this.photoSubscription = this.photoService.getPhotoByUsername(this.userProfileData.username).
+    getDownloadURL().subscribe(photo => {
+      this.userProfileData.photoUrl = photo;
+      this.loading = false;
+      let user: any = {
+        photoUrl: this.userProfileData.photoUrl
+      }
+      this.editPhotoProfile(this.userProfileData)
+
+      return this.userProfileData.photoUrl;
+    })
+  }
+
+  public editPhotoProfile(user) {
+    this.editPhotoSubscription = this.userService.editUser(this.userProfileData.username, user)
+    .subscribe()
+  }
+
+  public upload($event) {
+
+    this.photoService.postPhoto($event.target.files[0], this.userProfileData.username)
+    .then(()=>{
+      this.loading = true;
+      setTimeout(() => {
+        this.showPhoto();
+      }, 1000);
+    })
   }
 
   public existPhoto(): boolean {
@@ -119,6 +164,14 @@ export class ProfileComponent implements OnInit, OnDestroy {
 
     if (this.getPostsSubscriptor) {
       this.getPostsSubscriptor.unsubscribe();
+    }
+
+    if (this.photoSubscription) {
+      this.photoSubscription.unsubscribe();
+    }
+
+    if (this.editPhotoSubscription) {
+      this.editPhotoSubscription.unsubscribe();
     }
   }
 }
